917e22242946e9c7e453adc6bc9abdd592baf40a
Arthur Barbosa <arthur@Arthurs-Laptop.local>
2024-07-29 19:23:44 -0300

    Initial working with supermemory landing page

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,14 +1,62 @@
 import "@repo/tailwind-config/globals.css";
 
 import type { Metadata } from "next";
-import { ThemeScript } from "next-app-theme/theme-script";
 import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { cn } from "@repo/ui/lib/utils";
+import { Toaster } from "@repo/ui/shadcn/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const runtime = "edge";
+
 export const metadata: Metadata = {
-	title: "Arthur's Website",
-	description: "My Website",
+	title: "Supermemory - Your personal second brain.",
+	description:
+		"Bring saved information from all over the internet into one place where you can connect it, and ask AI about it",
+	openGraph: {
+		images: [
+			{
+				url: "https://supermemory.ai/og-image.png",
+				width: 1200,
+				height: 627,
+				alt: "Supermemory - Your personal second brain.",
+			},
+		],
+	},
+	metadataBase: {
+		host: "https://supermemory.ai",
+		href: "/",
+		origin: "https://supermemory.ai",
+		password: "supermemory",
+		hash: "supermemory",
+		pathname: "/",
+		search: "",
+		username: "supermemoryai",
+		hostname: "supermemory.ai",
+		port: "",
+		protocol: "https:",
+		searchParams: new URLSearchParams(""),
+		toString: () => "https://supermemory.ai/",
+		toJSON: () => "https://supermemory.ai/",
+	},
+	twitter: {
+		card: "summary_large_image",
+		site: "https://supermemory.ai",
+		creator: "https://supermemory.ai",
+		title: "Supermemory - Your personal second brain.",
+		description:
+			"Bring saved information from all over the internet into one place where you can connect it, and ask AI about it",
+		images: [
+			{
+				url: "https://supermemory.ai/og-image.png",
+				width: 1200,
+				height: 627,
+				alt: "Supermemory - Your personal second brain.",
+			},
+		],
+	},
 };
 
 export default function RootLayout({
@@ -17,11 +65,26 @@ export default function RootLayout({
 	children: React.ReactNode;
 }): JSX.Element {
 	return (
-		<html lang="en">
+		<html lang="en" className="overflow-x-hidden" suppressHydrationWarning>
 			<head>
-				<ThemeScript />
+				{/* Cloudflare web analytics */}
+				<script
+					defer
+					src="https://static.cloudflareinsights.com/beacon.min.js"
+					data-cf-beacon='{"token": "16d76ebb82c74d9983b71d09ab6617bc"}'
+				></script>
 			</head>
-			<body className={inter.className}>{children}</body>
+			{/* TODO: when lightmode support is added, remove the 'dark' class from the body tag */}
+			<body
+				className={cn(
+					`${inter.className} dark`,
+					GeistMono.variable,
+					GeistSans.variable,
+				)}
+			>
+				{children}
+				<Toaster />
+			</body>
 		</html>
 	);
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,118 +1,69 @@
-import { Button } from "@repo/ui/src/button";
-import { sql } from "drizzle-orm";
-import { getThemeToggler } from "./lib/get-theme-button";
-import { auth, signIn, signOut } from "./server/auth";
-import { db } from "./server/db";
-import { users } from "./server/db/schema";
+import RotatingIcons from "./RotatingIcons";
+import Hero from "./Hero";
+import { Navbar } from "./Navbar";
+import Cta from "./Cta";
+import { Toaster } from "@repo/ui/shadcn/toaster";
+import Features from "./Features";
+import Footer from "./footer";
+// import { auth } from "@/server/auth";
+import Services from "./Features/index";
+import { Showcases } from "./Showcase";
+import BackgroundPlus from "./GridPatterns/PlusGrid";
+import { redirect } from "next/navigation";
 
 export const runtime = "edge";
 
-export default async function Page() {
-	const usr = await auth();
+export default async function Home() {
+	// const user = await auth();
 
-	const userCount = await db
-		.select({
-			count: sql<number>`count(*)`.mapWith(Number),
-		})
-		.from(users);
-
-	const SetThemeButton = getThemeToggler();
+	// if (user) {
+	// 	await redirect("/home");
+	// }
 
 	return (
-		<main className="flex flex-col items-center justify-center min-h-screen">
-			<div className="flex max-w-2xl justify-between w-full">
-				<SetThemeButton />
+		<>
+			<BackgroundPlus />
+			<main className="flex overflow-x-hidden relative flex-col items-center px-2 min-h-screen md:px-0 font-geistSans bg-hero-gradient">
+				<div className="absolute top-0 -z-10 min-h-screen w-screen overflow-hidden bg-inherit  bg-[radial-gradient(ellipse_20%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
 
-				<div className="flex gap-2 items-center justify-center">
-					{" "}
-					<svg
-						viewBox="0 0 256 116"
-						xmlns="http://www.w3.org/2000/svg"
-						width="45px"
-						height="45px"
-						preserveAspectRatio="xMidYMid"
-						role="img"
-						aria-label="Cloudflare logo"
-					>
-						<path
-							fill="#FFF"
-							d="m202.357 49.394-5.311-2.124C172.085 103.434 72.786 69.289 66.81 85.997c-.996 11.286 54.227 2.146 93.706 4.059 12.039.583 18.076 9.671 12.964 24.484l10.069.031c11.615-36.209 48.683-17.73 50.232-29.68-2.545-7.857-42.601 0-31.425-35.497Z"
+				<Navbar />
+
+				{/* Background gradients */}
+				<div className="absolute top-0 left-0 w-full h-full z-[-1]">
+					<div className="overflow-x-hidden">
+						<div
+							className="absolute left-0 h-32 w-[95%] overflow-x-hidden bg-[#369DFD] bg-opacity-70 blur-[337.4px]"
+							style={{ transform: "rotate(-30deg)" }}
 						/>
-						<path
-							fill="#F4811F"
-							d="M176.332 108.348c1.593-5.31 1.062-10.622-1.593-13.809-2.656-3.187-6.374-5.31-11.154-5.842L71.17 87.634c-.531 0-1.062-.53-1.593-.53-.531-.532-.531-1.063 0-1.594.531-1.062 1.062-1.594 2.124-1.594l92.946-1.062c11.154-.53 22.839-9.56 27.087-20.182l5.312-13.809c0-.532.531-1.063 0-1.594C191.203 20.182 166.772 0 138.091 0 111.535 0 88.697 16.995 80.73 40.896c-5.311-3.718-11.684-5.843-19.12-5.31-12.747 1.061-22.838 11.683-24.432 24.43-.531 3.187 0 6.374.532 9.56C16.996 70.107 0 87.103 0 108.348c0 2.124 0 3.718.531 5.842 0 1.063 1.062 1.594 1.594 1.594h170.489c1.062 0 2.125-.53 2.125-1.594l1.593-5.842Z"
-						/>
-						<path
-							fill="#FAAD3F"
-							d="M205.544 48.863h-2.656c-.531 0-1.062.53-1.593 1.062l-3.718 12.747c-1.593 5.31-1.062 10.623 1.594 13.809 2.655 3.187 6.373 5.31 11.153 5.843l19.652 1.062c.53 0 1.062.53 1.593.53.53.532.53 1.063 0 1.594-.531 1.063-1.062 1.594-2.125 1.594l-20.182 1.062c-11.154.53-22.838 9.56-27.087 20.182l-1.063 4.78c-.531.532 0 1.594 1.063 1.594h70.108c1.062 0 1.593-.531 1.593-1.593 1.062-4.25 2.124-9.03 2.124-13.81 0-27.618-22.838-50.456-50.456-50.456"
-						/>
-					</svg>
-					<span className="italic">Cloudflare Next Saas Starter</span>
+					</div>
+
+					{/* a blue gradient line that's slightly tilted with blur (a lotof blur)*/}
+					{/* <div className="overflow-x-hidden overflow-y-hidden">
+          <div
+            className="absolute left-0 top-[100%] h-32 w-[90%] overflow-x-hidden bg-[rgb(54,157,253)] bg-opacity-40 blur-[337.4px]"
+            style={{ transform: "rotate(-30deg)" }}
+          />
+        </div> */}
+
+					{/* <div className="overflow-x-hidden overflow-y-hidden">
+          <div className="absolute right-0 top-[145%] h-40 w-[17%] overflow-x-hidden bg-[#369DFD] bg-opacity-20 blur-[110px]" />
+        </div> */}
 				</div>
 
-				<div className="border border-black dark:border-white rounded-2xl p-2 flex items-center">
-					Start by editing apps/web/page.tsx
-				</div>
-			</div>
+				{/* Hero section */}
+				<Hero />
+				<Showcases />
+				<Services />
 
-			<div className="max-w-2xl text-start w-full mt-16">
-				Welcome to Cloudflare Next Saas Starter. Testing<br /> Built a full stack app
-				using production-ready tools and frameworks, host on Cloudflare
-				instantly.
-				<br />
-				An opinionated, batteries-included framework with{" "}
-				<a
-					className="text-transparent bg-clip-text bg-gradient-to-r from-[#a93d64] to-[#275ba9]"
-					href="https://turbo.build"
-				>
-					Turborepo
-				</a>{" "}
-				and Nextjs. Fully Typesafe. Best practices followed by default.
-				<br /> <br />
-				Here's what the stack includes:
-				<ul className="list-disc mt-4 prose dark:prose-invert">
-					<li>
-						Authentication with <code>next-auth</code>
-					</li>
-					<li>Database using Cloudflare's D1 serverless databases</li>
-					<li>Drizzle ORM, already connected to your database and auth ⚡</li>
-					<li>Light/darkmode theming that works with server components (!)</li>
-					<li>Styling using TailwindCSS and ShadcnUI</li>
-					<li>Turborepo with a landing page and shared components</li>
-					<li>Cloudflare wrangler for quick functions on the edge</li>
-					<li>
-						... best part: everything's already set up for you. Just code!
-					</li>
-				</ul>
-				<div className="mt-4 flex flex-col gap-2">
-					<span>Number of users in database: {userCount[0]!.count}</span>
-				</div>
-				{usr?.user?.email ? (
-					<>
-						<div className="mt-4 flex flex-col gap-2">
-							<span>Hello {usr.user.name} 👋</span>
-							<span>{usr.user.email}</span>
-						</div>
-						<form
-							action={async () => {
-								"use server";
-								await signOut();
-							}}
-						>
-							<Button className="mt-4">Sign out</Button>
-						</form>
-					</>
-				) : (
-					<form
-						action={async () => {
-							"use server";
-							await signIn("google");
-						}}
-					>
-						<Button className="mt-4">Login with Google</Button>
-					</form>
-				)}
-			</div>
-		</main>
+				{/* Features section */}
+				<Features />
+
+				<RotatingIcons />
+				<Cta />
+
+				<Toaster />
+				<Footer />
+			</main>
+		</>
 	);
 }
